308e51c81610c27b70076701c04af6756b65918a
sandi <sandy.spyware@gmail.com>
2022-06-14 23:01:40 +0700

    Initial commit

---
+++ Example/MyCocoapodsLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyCocoapodsLibrary
+//
+//  Created by sandi on 06/14/2022.
+//  Copyright (c) 2022 sandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
