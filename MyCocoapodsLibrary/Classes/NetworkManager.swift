369f9835565ef84cc5e1944f74e13959bc8dabc1
sandi <sandy.spyware@gmail.com>
2022-06-15 08:12:56 +0700

    add networkmananger

---
+++ MyCocoapodsLibrary/Classes/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  MyCocoapodsLibrary
+//
+//  Created by SANDI on 15/06/22.
+//
+
+import Foundation
+
+public class NetworkManager {
+    
+    public init() {}
+    
+    public func PostData() {
+        print("For Post data")
+    }
+}
